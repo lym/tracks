1d54d7ba4e1e0c2dfdb50d59afba2984b94133b3
Salym Senyonga <salymsash@gmail.com>
2018-08-01 20:10:08 +0300

    Register headless_chrome driver

--- features/support/tracks_cucumber_settings.rb
+++ features/support/tracks_cucumber_settings.rb
@@ -23,4 +23,17 @@ if Capybara.javascript_driver == :selenium
     )
   end
   Capybara.javascript_driver = :selenium_english
+else
+  Capybara.register_driver(:headless_chrome) do |app|
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+      chromeOptions: { args: %w[headless disable-gpu] }
+    )
+
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      desired_capabilities: capabilities
+    )
+  end
+  Capybara.javascript_driver = :headless_chrome
 end
